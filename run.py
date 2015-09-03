b1ea4f86b0202b6fa75642cfd31d3dfc515a0523
dzikie drożdże <daz@hackerspace.pl>
2015-09-03 17:34:05 +0200

    dummy video driver

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+import os
+
+
 from soundboard.board import Board
 from soundboard.controls import Joystick
 
 
+
 def main():
+    os.environ['SDL_VIDEODRIVER'] = 'dummy'
     b = Board()
     b.register_sound_set('test')
     b.register_joystick(Joystick(0))
