d111c58fdd48b26596fd044d83fa09dbedbd6929
dzikie drożdże <daz@hackerspace.pl>
2015-09-16 21:46:51 +0200

    update test case

--- soundboard/tests/test_sound_set.py
+++ soundboard/tests/test_sound_set.py
@@ -3,5 +3,5 @@ from soundboard.sounds import SoundSet, SoundInterface
 
 def test_1():
     s = SoundSet('test')
-    assert len(s.sounds) == 2
+    assert len(s.sounds) == 4
     assert all(isinstance(s, SoundInterface) for s in s.sounds.values())
