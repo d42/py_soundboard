cbbbc1d74348ee2445aadd7094f57c439b09fb8b
dzikie drożdże <daz@hackerspace.pl>
2015-09-18 06:43:53 +0200

    Update tests

--- soundboard/tests/test_controls.py
+++ soundboard/tests/test_controls.py
@@ -1,5 +1,13 @@
-from soundboard.controls import Joystick
+import pytest
+from soundboard.controls import Joystick, RawSDLJoystick, RawEVDEVJoystick
+
+joystick_plugged_in = pytest.mark.joystick_plugged_in
 
 
+@joystick_plugged_in
 def test_sdl():
-    j = Joystick()
+    j = Joystick(0, backend=RawSDLJoystick)
+
+@joystick_plugged_in
+def test_evdev():
+    j = Joystick("/dev/input/event0", backend=RawEVDEVJoystick)
--- soundboard/tests/test_sounds.py
+++ soundboard/tests/test_sounds.py
@@ -1,11 +1,23 @@
-from soundboard.sounds import SimpleSound, VoxSound
+import pytest
+
+from soundboard import sounds
 from soundboard.mixer import NOPMixer
-from soundboard.utils import init_sdl
 
 
-def test_raw():
-    mixer = NOPMixer()
-    initialized = init_sdl()
-    assert initialized is True
-    s = SimpleSound("default/whip.wav", mixer=mixer)
-    # s = RawVox("is")
+@pytest.fixture
+def factory():
+    return sounds.SoundFactory(NOPMixer)
+
+
+def test_sounds(factory):
+    simple = factory.simple("test/test.wav")
+    simple.play()
+
+    vox = factory.vox("test")
+    vox.play()
+
+    list = factory.list(["test/test.wav", "test/test.wav"])
+    list.play()
+
+    random = factory.random(["test/test.wav", "test/test.wav"])
+    random.play()
