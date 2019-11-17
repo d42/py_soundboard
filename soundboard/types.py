e5104532859eaed514670961af23c42f0f6810d2
d42 <d42@users.noreply.github.com>
2019-11-17 16:56:20 +0100

    drop timestamped_event_tuple

--- soundboard/types.py
+++ soundboard/types.py
@@ -1,7 +1,6 @@
 from collections import namedtuple
 
 
-timestamped_event_tuple = namedtuple("timestamped_event_tuple", "button type timestamp")
 event_tuple = namedtuple("event_tuple", "button type")
 states_tuple = namedtuple("events_change_tuple", "pushed released held")
 sound_state = namedtuple("sound_state", "chunk position")
